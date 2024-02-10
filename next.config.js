fb04b62a7847767c3bdadf3a57ae781ef3244d9d
Azix <Azix1@outlook.fr>
2024-02-10 19:26:42 +0100

    remove webpack

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     domains: ['exemple.com', 'via.placeholder.com', 'ipfs-mainnet.trnnfr.com','ipfs-dev.trnnfr.com'], // Ajoutez ici les domaines de vos images
     deviceSizes: [82, 110, 140, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
+  webpack5: false,
 }
 
 module.exports = nextConfig
